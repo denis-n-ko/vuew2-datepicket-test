b7048413959c5bd775b8277cca7f391b0c822469
denis <super.denisko@gmail.com>
2022-05-29 21:35:56 +0000

    vue2 datepicker test

---
+++ hello-world/src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="app">
+    <date-picker v-model="date"></date-picker>
+  </div>
+</template>
+
+<script>
+import DatePicker from 'vue2-datepicker';
+import 'vue2-datepicker/index.css';
+
+export default {
+  name: 'App',
+  components: {
+    DatePicker
+  },
+  data() {
+    return {
+      date: new Date('2022-01-27 12:33:33')
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
